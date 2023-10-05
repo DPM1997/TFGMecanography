5f560a4d69ce3b2470a3b6f7afcc747f0ea3455a
David Pulido Marchal <david.pulido.marchal.997@gmail.com>
2023-10-05 15:01:19 +0200

    Modificadas las letras y los colliders de sitio
    Aparte modificado el script de los colliders para poder tener varias letras y que cuando se vaya una letra pues se elimine automaticamente

--- TFG/Assets/Scripts/Click/Click.cs
+++ TFG/Assets/Scripts/Click/Click.cs
@@ -7,7 +7,10 @@ using UnityEngine.UI;
 
 public class Click : MonoBehaviour
 {
-    [SerializeField] private UnityEngine.KeyCode keyCode;
+    [SerializeField] private UnityEngine.KeyCode keyCode1;
+    [SerializeField] private UnityEngine.KeyCode keyCode2;
+    [SerializeField] private UnityEngine.KeyCode keyCode3;
+    [SerializeField] private UnityEngine.KeyCode keyCode4;
     bool inside;
     Collider2D collisedObjectCollider;
     private TMP_Text text;
@@ -28,16 +31,19 @@ public class Click : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(keyCode) && inside == true){
+        if ((Input.GetKeyDown(keyCode1) || Input.GetKeyDown(keyCode2) || Input.GetKeyDown(keyCode3) || Input.GetKeyDown(keyCode4)) && inside == true){
           score = Int32.Parse(text.text);
-          score = score + 5;
+          Debug.Log("---inside:"+inside);
+          //Se mete en la función pero no modifica la variable del score
+          Debug.Log((Input.GetKeyDown(keyCode1) || Input.GetKeyDown(keyCode2) || Input.GetKeyDown(keyCode3) || Input.GetKeyDown(keyCode4) && inside == true));
+          score = score + 10;
           text.text=(""+score);
           Destroy(collisedObjectCollider.gameObject);
           //ReproducirSonido
           SoundFXScript.instance.PlayAudio(hitAudio,1f);
           collisedObjectCollider=null;
           inside=false;
-        } else if(Input.GetKeyDown(keyCode) && inside == false){
+        } else if((Input.GetKeyDown(keyCode1) || Input.GetKeyDown(keyCode2) || Input.GetKeyDown(keyCode3) || Input.GetKeyDown(keyCode4)) && inside == false){
             score = Int32.Parse(text.text);
             SoundFXScript.instance.PlayAudio(missAudio,1f);
             score = score -1;
@@ -48,17 +54,26 @@ public class Click : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag==keyCode.ToString())inside = true;
+        if(other.tag==keyCode1.ToString())inside = true;
+        else if(other.tag==keyCode2.ToString())inside = true;
+        else if(other.tag==keyCode3.ToString())inside = true;
+        else if(other.tag==keyCode4.ToString())inside = true;
         collisedObjectCollider=other;
     }
 
-
+    //Cuidado con esta función, cuando se destruye un objeto se detecta que se ha salido del trigger por lo que cuidadin
     void OnTriggerExit2D(Collider2D other)
     {
-        if(other.tag==keyCode.ToString())inside = false;
+        if(other.tag==keyCode1.ToString())inside = false;
+        else if(other.tag==keyCode1.ToString())inside = false;
+        else if(other.tag==keyCode1.ToString())inside = false;
+        else if(other.tag==keyCode1.ToString())inside = false;
         collisedObjectCollider=null;
         //Bajar vida
-
+        Destroy(other.gameObject);
+        score = Int32.Parse(text.text);
+        score = score - 5;
+        text.text=(""+score);
     }
 
 }
