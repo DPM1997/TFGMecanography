b96d1f0cf218102c0d8087b6e5ba43a9626b3fb0
David Pulido Marchal <david.pulido.marchal.997@gmail.com>
2024-05-13 18:28:43 +0200

    Modificado el colider y el script click.cs

--- TFG/Assets/Scripts/Click/Click.cs
+++ TFG/Assets/Scripts/Click/Click.cs
@@ -1,14 +1,13 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Click : MonoBehaviour
 {
-    [SerializeField] private UnityEngine.KeyCode keyCode1;
-    [SerializeField] private UnityEngine.KeyCode keyCode2;
-    [SerializeField] private UnityEngine.KeyCode keyCode3;
-    [SerializeField] private UnityEngine.KeyCode keyCode4;
+    [SerializeField] private List<KeyCode> keyCodeList;
+    [SerializeField] private List<GameObject> feedbackList;
     bool inside;
     Collider2D collisedObjectCollider;
     private TMP_Text scoreText;
@@ -37,41 +36,40 @@ public class Click : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKeyDown(keyCode1) || Input.GetKeyDown(keyCode2) || Input.GetKeyDown(keyCode3) || Input.GetKeyDown(keyCode4)) && inside == true){
-          //Se mete en la función pero no modifica la variable del score
-          scored=true;
-          Destroy(collisedObjectCollider.gameObject);
-          //ReproducirSonido
-          SoundFXScript.instance.PlayAudio(hitAudio,1f,MusicTypes.sfx);
-          collisedObjectCollider=null;
-          inside=false;
-        } else if((Input.GetKeyDown(keyCode1) || Input.GetKeyDown(keyCode2) || Input.GetKeyDown(keyCode3) || Input.GetKeyDown(keyCode4)) && inside == false){
-            score = Int32.Parse(scoreText.text);
-            SoundFXScript.instance.PlayAudio(missAudio,1f,MusicTypes.sfx);
-            score = score - 5;
-            scoreText.text=(""+score);
+        for (int i = 0; i < keyCodeList.Count; i++)
+        {
+            if (Input.GetKeyDown(keyCodeList[i])){
+                feedbackList[i].SetActive(true);
+                if(inside==true&&collisedObjectCollider.tag==keyCodeList[i].ToString()){
+                //Se mete en la función pero no modifica la variable del score
+                scored=true;
+                Destroy(collisedObjectCollider.gameObject);
+                //ReproducirSonido
+                SoundFXScript.instance.PlayAudio(hitAudio,1f,MusicTypes.sfx);
+                collisedObjectCollider=null;
+                inside=false;
+                }else{
+                SoundFXScript.instance.PlayAudio(missAudio,1f,MusicTypes.sfx);
+                score = score - 5;
+                scoreText.text=(""+score);
+                }
+            }
+            if (Input.GetKeyUp(keyCodeList[i]))
+                feedbackList[i].SetActive(false);
         }
-
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag==keyCode1.ToString())inside = true;
-        else if(other.tag==keyCode2.ToString())inside = true;
-        else if(other.tag==keyCode3.ToString())inside = true;
-        else if(other.tag==keyCode4.ToString())inside = true;
+        inside=true;
+        //Comprobamos la lista y lo metemos
         collisedObjectCollider=other;
     }
 
     //Cuidado con esta función, cuando se destruye un objeto se detecta que se ha salido del trigger por lo que cuidadin
     void OnTriggerExit2D(Collider2D other)
     {
-        combo = Int32.Parse(comboText.text);
-        score = Int32.Parse(scoreText.text);
-        if(other.tag==keyCode1.ToString())inside = false;
-        else if(other.tag==keyCode2.ToString())inside = false;
-        else if(other.tag==keyCode3.ToString())inside = false;
-        else if(other.tag==keyCode4.ToString())inside = false;
+        inside = false;
         if(scored==true){
             score = (int)(score+100+100*combo*0.01);
             scoreText.text=(""+score);
