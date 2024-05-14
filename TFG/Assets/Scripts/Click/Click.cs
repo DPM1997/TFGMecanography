8199c12dc99d92875d31c61bfedab44a47500c51
David Pulido Marchal <david.pulido.marchal.997@gmail.com>
2024-05-14 19:47:39 +0200

    Corregido por si hay varios objetos en el collider
    Lo del doble bool es una chapuza de campeonato

--- TFG/Assets/Scripts/Click/Click.cs
+++ TFG/Assets/Scripts/Click/Click.cs
@@ -8,27 +8,28 @@ public class Click : MonoBehaviour
 {
     [SerializeField] private List<KeyCode> keyCodeList;
     [SerializeField] private List<GameObject> feedbackList;
-    bool inside;
-    Collider2D collisedObjectCollider;
+    private List<Collider2D> collisedObjectColliderList;
     private TMP_Text scoreText;
     private TMP_Text comboText;
-    [SerializeField] private AudioClip hitAudio; 
-    [SerializeField] private AudioClip missAudio; 
+    [SerializeField] private AudioClip hitAudio;
+    [SerializeField] private AudioClip missAudio;
     public GameObject scoreObject;
     public GameObject comboObject;
     [SerializeField] private Slider slider;
     int score;
     int combo;
     bool scored;
+    bool scored2;
 
     // Start is called before the first frame update
     void Start()
     {
-        inside = false;
         score = 0;
-        scored=false;
-        combo=0;
-        slider.value=20;
+        scored = false;
+        scored2 = false;
+        combo = 0;
+        slider.value = 20;
+        collisedObjectColliderList =new List<Collider2D>();
         scoreText = scoreObject.GetComponent<TMP_Text>();
         comboText = comboObject.GetComponent<TMP_Text>();
     }
@@ -38,21 +39,30 @@ public class Click : MonoBehaviour
     {
         for (int i = 0; i < keyCodeList.Count; i++)
         {
-            if (Input.GetKeyDown(keyCodeList[i])){
+            if (Input.GetKeyDown(keyCodeList[i]))
+            {
                 feedbackList[i].SetActive(true);
-                if(inside==true&&collisedObjectCollider.tag==keyCodeList[i].ToString()){
-                //Se mete en la función pero no modifica la variable del score
-                scored=true;
-                Destroy(collisedObjectCollider.gameObject);
-                //ReproducirSonido
-                SoundFXScript.instance.PlayAudio(hitAudio,1f,MusicTypes.sfx);
-                collisedObjectCollider=null;
-                inside=false;
-                }else{
-                SoundFXScript.instance.PlayAudio(missAudio,1f,MusicTypes.sfx);
-                score = score - 5;
-                scoreText.text=(""+score);
+                for (int y = 0; y < collisedObjectColliderList.Count; y++)
+                {
+                    if (collisedObjectColliderList[y].tag == keyCodeList[i].ToString())
+                    {
+                        //Se mete en la función pero no modifica la variable del score
+                        scored = true;
+                        Collider2D destroyCollider = collisedObjectColliderList[y];
+                        collisedObjectColliderList.Remove(collisedObjectColliderList[y]);
+                        Destroy(destroyCollider.gameObject);
+                        //ReproducirSonido
+                        SoundFXScript.instance.PlayAudio(hitAudio, 1f, MusicTypes.sfx);
+                        scored2=true;
+                    }
                 }
+                if(scored==false&&scored2==false){
+                SoundFXScript.instance.PlayAudio(missAudio, 1f, MusicTypes.sfx);
+                score = score - 5;
+                scoreText.text = ("" + score);
+                }
+                if(scored2==true)
+                    scored2=false;
             }
             if (Input.GetKeyUp(keyCodeList[i]))
                 feedbackList[i].SetActive(false);
@@ -61,32 +71,32 @@ public class Click : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        inside=true;
-        //Comprobamos la lista y lo metemos
-        collisedObjectCollider=other;
+        collisedObjectColliderList.Add(other);
     }
 
     //Cuidado con esta función, cuando se destruye un objeto se detecta que se ha salido del trigger por lo que cuidadin
     void OnTriggerExit2D(Collider2D other)
     {
-        inside = false;
-        if(scored==true){
-            score = (int)(score+100+100*combo*0.01);
-            scoreText.text=(""+score);
+        collisedObjectColliderList.Remove(other);
+        if (scored == true)
+        {
+            score = (int)(score + 100 + 100 * combo * 0.01);
+            scoreText.text = ("" + score);
             combo++;
-            comboText.text=(""+combo);
-            if(combo%5==0)
+            comboText.text = ("" + combo);
+            if (combo % 5 == 0)
                 slider.value++;
-            scored=false;
-        }else{
-            combo=0;
-            comboText.text=(""+combo);
+            scored = false;
+        }
+        else
+        {
+            combo = 0;
+            comboText.text = ("" + combo);
             score = score - 20;
-            scoreText.text=(""+score);
+            scoreText.text = ("" + score);
             Destroy(other.gameObject);
             slider.value--;
         }
-        collisedObjectCollider=null;
     }
 
 }
